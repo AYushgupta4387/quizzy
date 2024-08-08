1a4c8a94cfc1c9891fc98a82d8fb1571ac057f0d
Ayush Gupta <ayush.gupta@zapcg.com>
2024-08-08 15:57:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="displayBlock"></div>
+      <div className="displayBlock"></div>
+      <div className="displayBlock"></div>
+      <div className="displayBlock"></div>
+    </div>
+  );
+}
+
+export default App;
